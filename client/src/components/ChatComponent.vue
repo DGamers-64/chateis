06eae3bbcc9586da5233ca70073712f06af55d38
Daniel Fuentes Vila <daniel.fuentes.vila@gmail.com>
2025-10-07 10:34:40 +0200

    Mensajes formateados correctamente y mostrados en el contenedor correspondiente

--- client/src/components/ChatComponent.vue
+++ client/src/components/ChatComponent.vue
@@ -1,9 +1,37 @@
 <template>
+    <div id="chat">
 
+    </div>
 </template>
 
 <script setup>
+import axios from 'axios'
 
+let historial = []
+setInterval(() => {
+    let url = 'http://localhost:7500/api/v1/mensajes'
+    if (historial.length > 0) {
+        const ultimoMensaje = historial.reduce((a, b) => 
+            new Date(a.mandado_en) > new Date(b.mandado_en) ? a : b
+        ).mandado_en
+        const fechaSQL = new Date(ultimoMensaje)
+            .toLocaleString('sv-SE', { hour12: false })
+        url += `?ultimo_timestamp=${encodeURIComponent(fechaSQL)}`
+    }
+    
+    axios.get(url)
+        .then(res => {
+            res.data.forEach(e => {
+                historial.push(e)
+                const fechaFormateada = new Date(e.mandado_en)
+                const fecha = `${fechaFormateada.getDate().toString().padStart(2, "0")}/${fechaFormateada.getMonth().toString().padStart(2, "0")}/${fechaFormateada.getFullYear().toString().padStart(2, "0")} ${fechaFormateada.getHours().toString().padStart(2, "0")}:${fechaFormateada.getMinutes().toString().padStart(2, "0")}`
+                document.getElementById("chat").innerHTML += `<p>(${fecha}) ${e.nombre}: ${e.mensaje}</p>`
+            });
+        })
+        .catch(err => {
+            console.error(err)
+        })
+}, 1000)
 </script>
 
 <style scoped>
