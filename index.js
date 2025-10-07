3e8e121eaeb7b6f8d066c3902abcc0e7ae9b1d50
Daniel Fuentes Vila <daniel.fuentes.vila@gmail.com>
2025-10-07 10:33:40 +0200

    Añadido CORS al servidor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { apiv1Router } from "./routers/v1/apiv1.js";
@@ -9,6 +10,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 7500;
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/api/v1", apiv1Router)
